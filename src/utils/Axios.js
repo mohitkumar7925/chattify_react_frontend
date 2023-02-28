8f0a3b18643e463eff087999451fdf77428e89f3
mohit <mohit@antiersolutions.com>
2023-02-28 15:19:39 +0530

    docker stable

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,8 +1,6 @@
-
-import axios from 'axios'
+import axios from "axios";
 export const Axios = axios.create({
-    // baseURL:'http://10.1.4.1:4000/user/',
-    baseURL:'http://192.168.42.247:4000/user/',
-    // baseURL:'http://localhost:4000/user/',
-    
-})
+      // baseURL:'http://10.1.4.1:4000/user/',
+      // baseURL:'http://192.168.42.247:4000/user/',
+      baseURL: "http://localhost:4000/user/",
+});
--- src/utils/EndPoints.js
+++ src/utils/EndPoints.js
@@ -1,3 +1,3 @@
 // export const Socket_URL = "http://10.1.4.1:4001"
-export const Socket_URL = "http://192.168.42.247:4001"
-// export const Socket_URL = "http://localhost:4001"
+// export const Socket_URL = "http://192.168.42.247:4001"
+export const Socket_URL = "http://localhost:4001";
